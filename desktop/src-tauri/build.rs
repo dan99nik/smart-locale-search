120707f4f65860b267f6a71b35afc379621d942e
Nikita Dankov <dan99nik@gmail.com>
2026-03-12 19:52:39 +0700

    fix: resolve CRT linking conflicts for Windows build

--- desktop/src-tauri/build.rs
+++ desktop/src-tauri/build.rs
@@ -1,3 +1,5 @@
 fn main() {
-  tauri_build::build()
+    println!("cargo:rustc-link-arg=/NODEFAULTLIB:libcpmt.lib");
+    println!("cargo:rustc-link-arg=/NODEFAULTLIB:libcpmtd.lib");
+    tauri_build::build()
 }
